634dacca3a8968ae7f3b3fea1e773e560a87fd34
HeartLoyalty <juannytoferreira@gmail.com>
2026-09-17 01:30:32 -0300

    adiciona atributo telefone no DTO

--- src/main/java/com/namassacompany/petVersoRestFull/dto/UsuarioPerfilDTO.java
+++ src/main/java/com/namassacompany/petVersoRestFull/dto/UsuarioPerfilDTO.java
@@ -9,10 +9,11 @@ public record UsuarioPerfilDTO(
         String nome,
         String apelido,
         String email,
+        String telefone,
         String fotoBase64
 ) {
     public UsuarioPerfilDTO(Usuario usuario){
-        this(usuario.getIdUsuario(), usuario.getNome(), usuario.getApelido(),usuario.getEmail(),
+        this(usuario.getIdUsuario(), usuario.getNome(), usuario.getApelido(),usuario.getEmail(),usuario.getTelefone(),
                 (usuario.getFoto() != null) ? Base64.getEncoder().encodeToString(usuario.getFoto()): null);
     }
 }
